20cdd16e08e1723a373126750abcda058ef3035e
chen-lao-wan <3225056883@qq.com>
2021-11-20 15:47:25 +0800

    添加项目文件。

---
+++ app.h
@@ -0,0 +1,12 @@
+#pragma once
+//导入需要的头文件s
+#include<iostream>
+#include<time.h>
+#include<Windows.h>
+//定义需要的变量
+int nub1, nub2;
+bool Sw;
+//定义坐标和移动坐标
+int Move[] = { 0,0 },Coordinates[] = { 0,0 };
+int Coordinates_X, Coordinates_Y;
+int Move_X,Move_Y ;
